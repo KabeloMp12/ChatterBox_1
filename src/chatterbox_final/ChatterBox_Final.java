5fa993f14b52c373bd2a4ba13e44d14d39e7924b
LENOVO <LENOVO@10.76.233.221>
2026-04-13 14:42:28 -1200

    Implemented username validation logic

--- src/chatterbox_final/ChatterBox_Final.java
+++ src/chatterbox_final/ChatterBox_Final.java
@@ -10,3 +10,7 @@ class Login {
     String StoreCellphonenumber; 
     String firstName;
     String lastName;
+
+ boolean checkUserName() {
+        return StoreUsername != null && StoreUsername.contains("_") && StoreUsername.length() <= 5;
+    }
